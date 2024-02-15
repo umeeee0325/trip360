a2e8ea7b0808b638282d8fe72b7a64ff4ffa7b97
umeeee0325 <nana19970204@icloud.com>
2024-02-15 20:06:54 +0900

    ActiveHushの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+  belongs_to :age
+  belongs_to :residence
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :profile, presence: true
+  validates :gender_id, :residence_id, :age_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
